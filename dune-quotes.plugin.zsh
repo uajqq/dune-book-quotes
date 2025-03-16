8065d18b183366b98f50008f4e88985de96027eb
uajqq <46248396+uajqq@users.noreply.github.com>
2025-03-16 14:20:53 -0400

    Init

--- dune-quotes.plugin.zsh
+++ dune-quotes.plugin.zsh
@@ -1,81 +1,99 @@
 dune_quotes=(
 
-	## Sources: Dune books (Frank Herbert) and film (David Lynch)
+	## Source: Dune (Frank Herbert)
 	## https://en.wikiquote.org/wiki/Dune
-	## https://en.wikiquote.org/wiki/Dune_(film)
 
-	#Paul Muad'Dib Atreides
-	"There is no escape — we pay for the violence of our ancestors.\n - Paul Muad'Dib Atreides"
-	"He who can destroy a thing, controls a thing.\n - Paul Muad'Dib Atreides"
-	"Long live the fighters!\n - Paul Muad’Dib Atreides"
-	"My name is a killing word.\n - Paul Muad’Dib Atreides"
-	"Father... father, the sleeper has awakened!\n - Paul Muad’Dib Atreides"
-	"Now remember, walk without rhythm, and we won't attract the worm.\n - Paul Muad’Dib Atreides"
-	"Are you suggesting the Duke's son is an animal?\n - Paul Muad’Dib Atreides"
-	"I will bend like a reed in the wind.\n - Paul Muad’Dib Atreides"
-
-	#Duke Leto Atreides
-	"I must rule with eye and claw — as the hawk among lesser birds.\n - Duke Leto Atreides"
-	"I'll miss the sea. But a man needs new experiences. They jar something deep inside, allowing him to grow. Without change, something sleeps inside of us and seldom awakens. The sleeper must awaken.\n - Duke Leto Atreides"
-	"The sleeper must awaken.\n - Duke Leto Atreides"
-
-	#Alia Atreides
-	"And how can this be? For he is the Kwisatz Haderach!\n - Alia Atreides"
-
-	#Mentat Thufir Hawat
-	"Those sounds could be imitated.\n - Mentat Thufir Hawat"
-	"Parting with friends is a sadness. A place is only a place.\n - Mentat Thufir Hawat"
-
-	#Gurney Hallek
-	"Not in the mood? Mood's a thing for cattle and loveplay, not fighting!\n - Gurney Halleck"
-	"If wishes were fishes, we'd all cast nets.\n - Gurney Halleck"
-	"Good. The slow blade penetrates the shield... but, uh, look down. We'd have joined each other in death.\n - Gurney Halleck"
-	"Soon we leave for Arrakis. Arrakis is real. The Harkonnens are real.\n - Gurney Halleck"
-
-	#Duncan Idaho
-	"May the Hand of God be with you.\n - Duncan Idaho"
-
-	#Dr. Liet-Kynes
-	"He shall know your ways as if born to them.\n - Dr. Liet-Kynes"
-	"Bless the maker and his water, bless the coming and going of him, may his passing cleanse the world.\n - Dr. Liet-Kynes"
-
-	#Fremen
-	"Bless the Maker and all His Water. Bless the coming and going of Him, May His passing cleanse the world. May He keep the world for his people. - Fremen Saying"
-	"May thy knife chip and shatter.\n - Fremen saying of ill will against an adversary; Jamis to Paul before their duel"
-
-	#Stilgar
-	"Usul, we have wormsign the likes of which even God has never seen.\n - Stilgar"
-	"To save one from a mistake is a gift of paradise.\n - Stilgar"
-
-	#Chani
-	"Tell me of your homeworld Usul.\n - Chani"
-	"I would not have permitted you to harm my tribe.\n - Chani"
-
-	#Bene Gesserit
+	"A beginning is the time for taking the most delicate care that the balances are correct. This every sister of the Bene Gesserit knows.\n - from Manual of Muad'Dib by the Princess Irulan"
+	"A popular man arouses the jealousy of the powerful.\n - Thufir Hawat"
+	"Parting with friends is a sadness. A place is only a place.\n - Thufir Hawat"
 	"I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.\n - Bene Gesserit Litany Against Fear"
-	"They tried and died.\n - Mother Gaius Helen Mohiam"
-
-	#Princess Irulan
-	"A beginning is a very delicate time.\n - Princess Irulan"
-	"God created Arrakis to train the faithful.\n - The Wisdom of Muad'Dib by the Princess Irulan"
-
-	#Baron Vladimir Harkonnen
-	"He who controls the Spice, controls the universe!\n - Baron Vladimir Harkonnen"
-
-	#Feyd-Rautha Harkonnen
-	"You see your death. My blade will finish you.\n - Feyd-Rautha Harkonnen"
-
-	#Mentat Piter De Vries
-	"It is by will alone I set my mind in motion. It is by the juice of sapho that thoughts acquire speed, the lips acquire stains, the stains become a warning. It is by will alone I set my mind in motion. \n - The Mentat Mantra, recited by Piter De Vries"
-	"Vendetta, he says, using the ancient tongue. The art of kanly is still alive in the Universe.\n - Mentat Piter De Vries"
-	"Take them to the desert as the traitor suggested. The worms will destroy the evidence. Their bodies must never be found.\n - Mentat Piter De Vries"
-
-	#The Guild
-	"A Secret Report within the Guild. Four Planets have come to our attention regarding a plot which could jeopardise Spice Production: Planet Arrakis, Source of the Spice. Planet Caladan, home of House Atreides. Planet Geidi Prime, home of House Harkonnen. Planet Kaitain, Home of the Emperor of the Known Universe. Send a third-stage Guild Navigator to Kaitain to demand details from the Emperor. The Spice must flow.\n - The Guildmaster"
-	"The Bene Gesserit witch must leave.\n - Guildsman" 
-
-	#Frank Herbert
-	"Muad’Dib learned rapidly because his first training was in how to learn.\n - Frank Herbert - Dune"
+	"You've heard of animals chewing off a leg to escape a trap? There's an animal kind of trick. A human would remain in the trap, endure the pain, feigning death that he might kill the trapper and remove a threat to his kind.\n - Reverend Mother Gaius Helen Mohiam"
+	"Hope clouds observation.\n - Reverend Mother Gaius Helen Mohiam"
+	"Once men turned their thinking over to machines in the hope that this would set them free. But that only permitted other men with machines to enslave them.\n - Reverend Mother Gaius Helen Mohiam"
+	"They tried and died.\n - Reverend Mother Gaius Helen Mohiam"
+	"In politics, the tripod is the most unstable of all structures.\n - Reverend Mother Gaius Helen Mohiam"
+	"That which submits rules....The willow submits to the wind and prospers until one day it is many willows — a wall against the wind. This is the willow's purpose.\n - Reverend Mother Gaius Helen Mohiam"
+	"A world is supported by four things...the learning of the wise, the justice of the great, the prayers of the righteous and the valor of the brave. But all of these are as nothing... without a ruler who knows the art of ruling. Make that the science of your tradition!\n - Reverend Mother Gaius Helen Mohiam"
+	"The mystery of life isn't a problem to solve, but a reality to experience.\n - Reverend Mother Gaius Helen Mohiam"
+	"A process cannot be understood by stopping it. Understanding must move with the flow of the process, must join it and flow with it.\n - The First Law of Mentat"
+	"In shield fighting, one moves fast on defense, slow on attack. Attack has the sole purpose of tricking the opponent into a misstep, setting him up for the attack sinister. The shield turns the fast blow, admits the slow kindjal!\n - Gurney Halleck"
+	"Mood? What has mood to do with it? You fight when the necessity arises — no matter the mood! Mood's a thing for cattle or making love or playing the baliset. It's not for fighting.\n - Gurney Halleck"
+	"If wishes were fishes, we'd all cast nets.\n - Gurney Halleck"
+	"Polish comes from the cities; wisdom from the desert.\n - Arrakeen villager saying"
+	"Think you of the fact that a deaf person cannot hear. Then, what deafness may we not all possess? What senses do we lack that we cannot see and cannot hear another world all around us?\n - The Orange Catholic Bible"
+	"Muad'Dib learned rapidly because his first training was in how to learn. And the first lesson of all was the basic trust that he could learn. It's shocking to find how many people do not believe they can learn, and how many more believe learning to be difficult. Muad'Dib knew that every experience carries its lesson.\n - from The Humanity of Muad'Dib by the Princess Irulan"
+	"Any road followed precisely to its end leads precisely nowhere. Climb the mountain just a little bit to test that it's a mountain. From the top of the mountain, you cannot see the mountain.\n - from Muad'Dib: Family Commentaries by the Princess Irulan"
+	"O you who know what we suffer here, do not forget us in your prayers.\n - from Manual of Muad'Dib by the Princess Irulan"
+	"Let us not rail about justice as long as we have arms and the freedom to use them.\n - Duke Leto Atreides"
+	"Sir, I honor and respect the personal dignity of any man who respects my dignity.\n - Duke Leto Atreides"
+	"I must rule with eye and claw — as the hawk among lesser birds.\n - Duke Leto Atreides"
+	"Without change something sleeps inside us, and seldom awakens. The sleeper must awaken.\n - Duke Leto Atreides"
+	"There is probably no more terrible instance of enlightenment than the one in which you discover your father is a man — with human flesh.\n - from Collected Sayings of Muad’Dib by the Princess Irulan"
+	"It is impossible to live in the past, difficult to live in the present and a waste to live in the future."
+	"He shall know your ways as if born to them.\n - Prophecy of the Mahdi"
+	"You should fear me, Mother. I am the Kwisatz Haderach.\n - Paul Muad'Dib Atreides"
+	"Bless the Maker and all His Water. Bless the coming and going of Him, May His passing cleanse the world. May He keep the world for his people.\n - Liet-Kynes"
+	"You never talk of likelihoods on Arrakis. You speak only of possibilities.\n - Liet-Kynes"
+	"When God hath ordained a creature to die in a particular place, He causeth that creature's wants to direct him to that place.\n - The Orange Catholic Bible"
+	"Greatness is a transitory experience. It is never persistent. It depends in part upon the myth-making imagination of humankind. The person who experiences greatness must have a feeling for the myth he is in. He must reflect what is projected upon him. And he must have a strong sense of the sardonic. This is what uncouples him from belief in his own pretensions. The sardonic is all that permits him to move within himself. Without this quality, even occasional greatness will destroy a man.\n - from Collected Sayings of Muad’Dib by the Princess Irulan"
+	"There is no escape — we pay for the violence of our ancestors.\n - Paul Muad'Dib Atreides"
+	"Yes. They’ll call me Muad’Dib, the one who points the way. Yes that’s what they’ll call me.\n - Paul Atreides"
+	"Knowing where the trap is, that's the first step in evading it.\n - Thufir Hawat"
+	"Thou shalt not make a machine in the likeness of a human mind.\n - The Orange Catholic Bible"
+	"The vision of time is broad, but when you pass through it, time becomes a narrow door.\n - from Arrakis Awakening by the Princess Irulan" 
+	"A man's flesh is his own; the water belongs to the tribe.\n - Fremen proverb"
+	"Is it defeatist or treacherous for a doctor to diagnose a disease correctly? My only intention is to cure the disease.\n - Lady Jessica"
+	"There should be a science of discontent. People need hard times and oppression to develop psychic muscles.\n - from Collected Sayings of Muad'Dib by the Princess Irulan"
+	"Arrakis teaches the attitude of the knife — chopping off what's incomplete and saying: 'Now it's complete because it's ended here.'\n - from Collected Sayings of Muad'Dib by the Princess Irulan"
+	"Be prepared to appreciate what you meet.\n - Fremen proverb"
+	"There's steel in this man that no one has taken the temper out of.\n - Lady Jessica"
+	"My lungs taste the air of time, blown past falling sands.\n - The Orange Catholic Bible"
+	"Beginnings are such delicate times.\n - Lady Jessica"
+	"One should never presume one is the sole object of a hunt.\n - Fremen saying"
+	"A plan depends as much upon execution as it does upon concept.\n - Lady Jessica"
+	"Delay is as dangerous as the wrong answer.\n - Lady Jessica"
+	"The knife is more dangerous than the hand and the knife can be in either hand.\n - Gurney Halleck"
+	"Humans live best when each has his own place, when each knows where he belongs in the scheme of things. Destroy the place and destroy the person.\n - Lady Jessica"
+	"One must always keep the tools of statecraft sharp and ready. Power and fear - sharp and ready.\n - Baron Vladimir Harkonnen"
+	"Do not count a human dead until you've seen his body. And even then you can make a mistake.\n - Bene Gesserit saying"
+	"I see us giving love to each other in a time of quiet between storms. It's what we were meant to do.\n - Chani"
+	"My father once told me that respect for the truth comes close to being the basis for all morality. 'Something cannot emerge from nothing,' he said. This is profound thinking if you understand how unstable 'the truth' can be.\n - from Conversations with Muad'Dib by the Princess Irulan"
+	"Muad'Dib could indeed see the Future, but you must understand the limits of this power. Think of sight. You have eyes, yet cannot see without light. If you are on the floor of a valley, you cannot see beyond your valley. Just so, Muad'Dib could not always choose to look across the mysterious terrain. He tells us that a single obscure decision of prophecy, perhaps the choice of one word over another, could change the entire aspect of the future. He tells us 'The vision of time is broad, but when you pass through it, time becomes a narrow door.' And always, he fought the temptation to choose a clear, safe course, warning 'That path leads ever down into stagnation.'\n - from Arrakis Awakening by the Princess Irulan"
+	"What do you despise? By this you are truly known.\n - from Manual of Muad'Dib by the Princess Irulan"
+	"A stone is heavy and the sand is weighty; but a fools wrath is heavier than them both.\n - Esmar Tuek"
+	"Whether a thought is spoken or not it is a real thing and it has powers of reality.\n - Esmar Tuek"
+	"Proper teaching is recognized with ease. You can know it without fail because it awakens within you that sensation which tells you this is something you have always known."
+	"The mind can go either direction under stress — toward positive or toward negative: on or off. Think of it as a spectrum whose extremes are unconsciousness at the negative end and hyperconsciousness at the positive end. The way the mind will lean under stress is strongly influenced by training.\n - Bene Gesserit axiom"
+	"The highest function of ecology is understanding consequences.\n - Pardot Kynes"
+	"Science is made up of so many things that appear obvious after they are explained.\n - Pardot Kynes"
+	"No more terrible disaster could befall your people than for them to fall into the hands of a Hero.\n - Pardot Kynes"
+	"Then, as his planet killed him, it occurred to Kynes that his father and all the other scientists were wrong, that the most persistent principles of the universe were accident and error.\n"
+	"Prophecy and prescience — How can they be put to the test in the face of the unanswered questions? Consider: How much is actual prediction of the "wave form" (as Muad'Dib referred to his vision-image) and how much is the prophet shaping the future to fit the prophecy? What of the harmonics inherent in the act of prophecy? Does the prophet see the future or does he see a line of weakness, a fault or cleavage that he may shatter with words or decisions as a diamond-cutter shatters his gem with a blow of a knife?\n - Private Reflections on Muad'Dib by the Princess Irulan"
+	"To save one from a mistake is a gift of paradise./n - Stilgar"
+	"A killer with the manners of a rabbit - this is the most dangerous kind.\n - Baron Vladimir Harkonnen"
+	"The Fremen were supreme in that quality the ancients called 'spannungsbogen' — which is the self-imposed delay between desire for a thing and the act of reaching out to grasp that thing.\n - from The Wisdom of Muad'Dib by the Princess Irulan"
+	"The vision made him want to freeze into immobility, but this, too, was action with its consequences."
+	"My mother obeyed her Sister Superiors where the Lady Jessica disobeyed. Which of them was the stronger? History already has answered.\n - from In My Father's House by the Princess Irulan"
+	"May thy knife chip and shatter.\n - Fremen saying"
+	"God created Arrakis to train the faithful.\n - from The Wisdom of Muad'Dib by the Princess Irulan"
+	"The concept of progress acts as a protective mechanism to shield us from the terrors of the future.\n - from Collected Sayings of Muad'Dib by the Princess Irulan"
+	"It's easier to be terrified by an enemy you admire.\n - Thufir Hawat"
+	"To accept a little death is worse than death itself.\n - Chani"
+	"You cannot back into the future.\n - Paul Muad'Dib Atreides"
+	"Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic.\n - from The Sayings of Muad'Dib by the Princess Irulan"
+	"'Give as few orders as possible,' his father had told him…once…long ago. 'Once you've given orders on a subject, you must always give orders on that subject.' The Fremen knew this rule instinctively."
+	"When religion and politics travel in the same cart, the riders believe nothing can stand in their way. Their movement becomes headlong – faster and faster and faster. They put aside all thought of obstacles and forget that a precipice does not show itself to the man in a blind rush until it's too late.\n - Bene Gesserit proverb"
+	"'Control the coinage and the courts — let the rabble have the rest.' Thus the Padishah Emperor advises you. And he tells you; 'If you want profits, you must rule.' There is truth in these words, but I ask myself: 'Who are the rabble and who are the ruled?'\n - Muad'Dib's Secret Message to the Landsraad from Arrakis Awakening by the Princess Irulan"
+	"When law and duty are one, united by religion, you never become fully conscious, fully aware of yourself. You are always a little less than an individual.\n - from Muad'Dib: The Ninety-nine Wonders of The Universe by the Princess Irulan"
+	"How often it is that the angry man rages denial of what his inner self is telling him.\n - The Collected Sayings of Muad'Dib by the Princess Irulan"
+	"The eye that looks ahead to the safe course is closed forever.\n - Paul Muad'Dib Atreides"
+	"The power to destroy a thing is the absolute control over it.\n - Paul Muad'Dib Atreides"
+	"Try looking into that place where you dare not look! You'll find me there, staring out at you!\n - Paul Muad'Dib Atreides"
+	"Expect only what happens in the fight. That way you'll never be surprised.\n - Duncan Idaho"
+	"Muad'Dib is wise in the ways of the desert. Muad'Dib creates his own water. Muad'Dib hides from the sun and travels in the cool night. Muad'Dib is fruitful and multiplies over the land. Muad'Dib we call 'instructor-of-boys.' That is a powerful base on which to build your life, Paul-Muad'Dib, who is Usul among us. We welcome you.\n - Stilgar"
+	"For when youth and beauty have gone, she will find that the place-between, once occupied by tension, has become a wellspring of cunning and resourcefulness.\n - from Muad'Dib, Family Commentaries by the Princess Irulan"
+	"I'll sheath my knife in your blood.\n - Feyd-Rautha Harkonnen"
+	"Observe the plans within plans within plans.\n - Baron Vladimir Harkonnen"
 
 )
 # "\e[1m" = bold
